bf1273b5f50685a4e0b67f3abc9ef75c31ce711a
iphyosege <101004580+iphyosege@users.noreply.github.com>
2022-04-05 20:47:36 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+//program for a simple calculator
+//take an operator
+var firstNumber=prompt("Enter the firstNumber")
+var secondNumber=prompt("Enter the secondNumber")
+//result using declaration of function
+//program to add two numbers
+function add(fisrtNumber,secondNumber) {
+  return firstNumber+secondNumber;
+  alert((result = add(Number(firstNumber),Number(secondNumber)));
+}
+//result using declaration of function
+//program to subtract two numbers
+function subtract(fisrtNumber,secondNumber) {
+  return firstNumber-secondNumber;
+  alert((result = add(Number(firstNumber),Number(secondNumber)));
+}
+//result using declaration of function
+//program to add two numbers
+function division(fisrtNumber,secondNumber) {
+  return firstNumber/secondNumber;
+  alert((result = divide(Number(firstNumber),Number(secondNumber)));
+}
+//result using declaration of function
+//program to add two numbers
+function multiply(fisrtNumber,secondNumber) {
+  return firstNumber*secondNumber;
+  alert((result = multiply(Number(firstNumber),Number(secondNumber)));
+}
